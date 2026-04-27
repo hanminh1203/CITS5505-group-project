527d8c3e5bb95c777dd7300c73bca84d973e8994
Mark Tran <tranhanminh34@gmail.com>
2026-04-27 17:12:46 +0800

    add flake8 into local environment

--- setenv.bat
+++ setenv.bat
@@ -4,6 +4,8 @@ DOSKEY act_env=venv\Scripts\activate
 DOSKEY run_dev=flask --app run run --host=0.0.0.0 --debug
 DOSKEY freeze_version=pip freeze ^> requirements.txt
 DOSKEY dummy=python -m scripts.seed_dummy_data
+DOSKEY lint1=flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+DOSKEY lint2=flake8 . --count --max-complexity=10 --max-line-length=127 --statistics
 
 DOSKEY db_init=flask db init
 DOSKEY db_migrate=flask db migrate -m $*
