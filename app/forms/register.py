ba8924e8b0572b4de79b65a86600931d989b7cf6
WANG JIANING <zzzzzhazha1004@gmail.com>
2026-04-30 23:53:05 +0800

    Add register form

---
+++ app/forms/register.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+
+class RegisterForm(FlaskForm):
+    name = StringField(
+        "Name",
+        validators=[
+            DataRequired(),
+            Length(min=3, message="Name must be at least 3 characters."),
+        ],
+    )
+    email = StringField(
+        "Email",
+        validators=[
+            DataRequired(),
+            Email(message="Please enter a valid email address."),
+        ],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            Length(min=8, message="Password must be at least 8 characters."),
+        ],
+    )
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[
+            DataRequired(),
+            EqualTo("password", message="Confirm password must match password."),
+        ],
+    )
