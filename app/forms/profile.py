19180cfc36da9817b2a5ada0ae608ecb3e258b1a
misuchii <919949918@qq.com>
2026-05-08 16:53:32 +0800

    e501

--- app/forms/profile.py
+++ app/forms/profile.py
@@ -5,8 +5,11 @@ from wtforms.validators import DataRequired, Length
 
 class ProfileForm(FlaskForm):
     name = StringField(
-        "Display name", validators=[DataRequired(), 
-        Length(min=3, max=255, message="Name is at least 3 characters")]
+        "Display name",
+        validators=[
+            DataRequired(),
+            Length(min=3, max=255, message="Name is at least 3 characters"),
+        ],
     )
     bio = TextAreaField("Bio")
     address = StringField("Location", validators=[Length(max=255)])
