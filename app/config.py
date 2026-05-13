20507709ff6b718cba730dc35e82de057aa8229d
Mark Tran <tranhanminh34@gmail.com>
2026-05-13 21:16:59 +0800

    revert change on config as tests does not use this

--- app/config.py
+++ app/config.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-from app.exceptions import IllegalArgumentException
-
 # Load environment variables from .env file
 load_dotenv()
 
@@ -14,14 +12,6 @@ def get_bool_env(name, default=False):
     return value.strip().lower() in {"1", "true", "yes", "on"}
 
 
-def get_required(name):
-    value = os.environ.get(name)
-    if value is None:
-        msg = f"Environment variable '{name}' is missing"
-        raise IllegalArgumentException(msg)
-    return value.strip()
-
-
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'SQLALCHEMY_DATABASE_URI',
@@ -30,6 +20,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = get_bool_env('SQLALCHEMY_ECHO', False)
     FLASK_DEBUG = get_bool_env('FLASK_DEBUG', False)
-    SECRET_KEY = get_required('SECRET_KEY')
-    WTF_CSRF_SECRET_KEY = get_required('WTF_CSRF_SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY')
     REQUESTS_PAGE_SIZE = int(os.getenv("REQUESTS_PAGE_SIZE", 6))
